893de709ae35b3d3f5943cf1db65ab98b3e3350c
Marc Philipp <mail@marcphilipp.de>
2021-04-03 15:18:42 +0200

    JAXB

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.gradle.api.JavaVersion.VERSION_1_8
 
 plugins {
     `java-library`
+    id("org.unbroken-dome.xjc") version "2.0.0"
 }
 
 java {
@@ -16,6 +17,9 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
     implementation("info.picocli:picocli:4.6.1")
+    implementation("jakarta.xml.bind:jakarta.xml.bind-api:3.0.0")
+    xjcTool("com.sun.xml.bind:jaxb-xjc:3.0.0")
+    xjcTool("com.sun.xml.bind:jaxb-impl:3.0.0")
 }
 
 tasks {
@@ -24,5 +28,15 @@ tasks {
     }
     test {
         useJUnitPlatform()
+    }
+}
+
+xjc {
+    xjcVersion.set("3.0")
+}
+
+sourceSets {
+    main {
+        xjcTargetPackage.set("org.opentest4j.reporting.xml")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,1 @@
 rootProject.name = "open-test-reporting"
-
-include("lib")
