55363528a1e309ea4619284491b44b745ef9fb1f
Marc Philipp <mail@marcphilipp.de>
2022-02-06 15:21:37 +0100

    Add Javadoc to schema subproject

--- schema/build.gradle.kts
+++ schema/build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     `java-conventions`
 }
+
+tasks {
+    javadoc {
+        (options as StandardJavadocDocletOptions).apply {
+            addBooleanOption("Werror", true)
+            addBooleanOption("Xdoclint:all", true)
+        }
+    }
+}
