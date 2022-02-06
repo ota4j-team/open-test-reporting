21b41f8ad8e4ffc7f4d569cc4985dcd3af6d88a3
Marc Philipp <mail@marcphilipp.de>
2022-02-06 15:29:44 +0100

    Add Javadoc to cli subproject

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -20,4 +20,10 @@ tasks {
     compileJava {
         options.release.set(11)
     }
+    javadoc {
+        (options as StandardJavadocDocletOptions).apply {
+            addBooleanOption("Werror", true)
+            addBooleanOption("Xdoclint:all", true)
+        }
+    }
 }
