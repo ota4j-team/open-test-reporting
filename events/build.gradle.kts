d77c914059f73ac40ba1386021591ab2d26ab18d
Marc Philipp <mail@marcphilipp.de>
2024-04-19 07:56:57 +0200

    Add separate test task to test against Woodstox StAX implementation

--- events/build.gradle.kts
+++ events/build.gradle.kts
@@ -2,10 +2,29 @@ plugins {
     `java-conventions`
 }
 
+val woodstox = configurations.dependencyScope("woodstox")
+val woodstoxRuntimeClasspath = configurations.resolvable("woodstoxRuntimeClasspath") {
+    extendsFrom(configurations.testRuntimeClasspath.get())
+    extendsFrom(woodstox.get())
+}
+
 dependencies {
     api(projects.schema)
     testImplementation(libs.archunit)
     testImplementation(libs.assertj.core)
     testImplementation(libs.bundles.junit)
     testImplementation(libs.xmlunit.assertj)
+    woodstox(libs.woodstox.core)
+}
+
+tasks {
+    val testWoodstox by registering(Test::class) {
+        val test by testing.suites.existing(JvmTestSuite::class)
+        testClassesDirs = files(test.map { it.sources.output.classesDirs })
+        classpath = files(sourceSets.main.map { it.output }) + files(test.map { it.sources.output }) + woodstoxRuntimeClasspath.get()
+        group = JavaBasePlugin.VERIFICATION_GROUP
+    }
+    check {
+        dependsOn(testWoodstox)
+    }
 }
