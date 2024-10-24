b9f87e90891c2706d11a43b7b25ed1c9b696e314
Marc Philipp <mail@marcphilipp.de>
2024-10-24 16:29:19 +0200

    Allow selecting nodes

--- html-report/src/App.vue
+++ html-report/src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import ExecutionSummary from './components/ExecutionSummary.vue'
+import NodeDetails from './components/NodeDetails.vue';
 import TestTree from './components/TestTree.vue';
+import { ref } from 'vue';
 
 defineProps<{ testCount: number, roots: Array<TestNode> }>()
+const selectedNode = ref(undefined as TestNode | undefined)
 </script>
 
 <template>
@@ -10,13 +13,15 @@ defineProps<{ testCount: number, roots: Array<TestNode> }>()
     <div class="flex bg-green-600 h-10 p-2 border-b-4 border-green-700 text-white">
       <ExecutionSummary :testCount="testCount" />
     </div>
-    <div class="flex flex-1 overflow-hidden">
+    <div class="flex flex-1">
       <div class="flex bg-gray-50 w-1/4 min-w-80 p-4 overflow-y-auto text-sm">
-        <TestTree :roots="roots"/>
+        <TestTree :roots="roots" v-model:selectedNode="selectedNode" class="-ml-6"/>
       </div>
       <div class="flex flex-1 flex-col">
         <div class="flex flex-1 overflow-y-auto p-4">
-          <p>Content</p>
+          <div>
+            <NodeDetails :node="selectedNode" v-if="selectedNode"/>
+          </div>
         </div>
       </div>
     </div>
