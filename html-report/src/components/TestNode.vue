9ef2b04bdd659e42aa836a8c882cbcbd0db5a608
Marc Philipp <mail@marcphilipp.de>
2024-10-24 16:29:19 +0200

    Allow selecting nodes

--- html-report/src/components/TestNode.vue
+++ html-report/src/components/TestNode.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import TestTree from './TestTree.vue';
 
-defineProps<{ node: TestNode }>()
+const selectedNode = defineModel('selectedNode')
+const props = defineProps<{ node: TestNode }>()
 const showChildren = ref(true)
 function toggleChildren() {
   showChildren.value = !showChildren.value
 }
+function selectNode() {
+  selectedNode.value = props.node
+}
 </script>
 
 <template>
-  <span @click="toggleChildren">{{ node.name }}</span>
-  <ul v-if="node.child && showChildren" class="list-disc list-inside pl-4">
-    <li v-for="c in node.child" class="pt-1">
-      <TestNode :node="c"/>
-    </li>
-  </ul>
+  <button class="rounded px-2 bg-gray-200 mr-1" @click="toggleChildren" v-if="node.child"><span v-if="showChildren">-</span><span v-else>+</span></button>
+  <span class="cursor-pointer rounded p-px px-1" :class="{ 'bg-gray-300': selectedNode !== undefined && (selectedNode as TestNode).name == node.name }" @click="selectNode">{{ node.name }}</span>
+  <TestTree :roots="node.child" v-model:selectedNode="selectedNode" v-if="node.child" :class="{ hidden: !showChildren }"/>
 </template>
 
 <style scoped>
