3587e6c8a09be2a2abb0fad56e0fbef56b190b76
Marc Philipp <mail@marcphilipp.de>
2024-10-24 16:29:19 +0200

    Allow selecting nodes

---
+++ html-report/src/components/NodeDetails.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{ node: TestNode }>()
+</script>
+
+<template>
+  <h2 class="text-xl mb-4">{{ node.name }}</h2>
+  <div><pre class="text-xs rounded-lg shadow-inner p-4 h-128 w-1/2 overflow-scroll">{{ node }}</pre></div>
+</template>
+
+<style scoped>
+</style>
--- html-report/src/components/TestTree.vue
+++ html-report/src/components/TestTree.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import TestNode from './TestNode.vue';
 
+const selectedNode = defineModel('selectedNode')
 defineProps<{ roots: Array<TestNode> }>()
 </script>
 
 <template>
-  <ul v-if="roots.length" class="list-disc list-inside">
-    <li v-for="root in roots" class="pt-1">
-      <TestNode :node="root"/>
+  <ul v-if="roots.length" class="list-disc list-inside pl-5">
+    <li v-for="root in roots" class="pt-0.5">
+      <TestNode :node="root" v-model:selectedNode="selectedNode"/>
     </li>
   </ul>
 </template>
