b8074d53b6f535b38c36a3141f3d8de50f14ff68
Marc Philipp <mail@marcphilipp.de>
2024-10-24 09:55:17 +0200

    Display test tree

---
+++ html-report/src/components/TestTree.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TestNode from './TestNode.vue';
+
+defineProps<{ roots: Array<TestNode> }>()
+</script>
+
+<template>
+  <ul v-if="roots.length">
+    <li v-for="root in roots">
+      <TestNode :node="root"/>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+</style>
