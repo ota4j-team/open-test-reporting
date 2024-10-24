85634cfcd45b43c9cbf79ac9b58172b64c110af9
Marc Philipp <mail@marcphilipp.de>
2024-10-24 09:55:17 +0200

    Display test tree

---
+++ html-report/src/components/TestNode.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ node: TestNode }>()
+</script>
+
+<template>
+  <span>{{ node.name }}</span>
+  <ul v-if="node.child">
+    <li v-for="c in node.child">
+      <TestNode :node="c"/>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+</style>
