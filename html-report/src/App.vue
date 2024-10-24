5b7ce1a2a301b297cece1bb75e6f6dd9a17d2920
Marc Philipp <mail@marcphilipp.de>
2024-10-24 14:16:15 +0200

    Basic layout

--- html-report/src/App.vue
+++ html-report/src/App.vue
@@ -6,8 +6,21 @@ defineProps<{ testCount: number, roots: Array<TestNode> }>()
 </script>
 
 <template>
-  <ExecutionSummary :testCount="testCount" />
-  <TestTree :roots="roots"/>
+  <main class="flex flex-col h-screen">
+    <div class="flex bg-green-600 h-10 p-2 border-b-4 border-green-700 text-white">
+      <ExecutionSummary :testCount="testCount" />
+    </div>
+    <div class="flex flex-1 overflow-hidden">
+      <div class="flex bg-gray-50 w-1/4 min-w-80 p-4 overflow-y-auto text-sm">
+        <TestTree :roots="roots"/>
+      </div>
+      <div class="flex flex-1 flex-col">
+        <div class="flex flex-1 overflow-y-auto p-4">
+          <p>Content</p>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
