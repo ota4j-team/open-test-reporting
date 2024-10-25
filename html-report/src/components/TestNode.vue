38756ddfdb31e967b8af9e9abfa762fc64e68cd9
Marc Philipp <mail@marcphilipp.de>
2024-10-25 15:40:24 +0200

    Polishing

--- html-report/src/components/TestNode.vue
+++ html-report/src/components/TestNode.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import TestTree from './TestTree.vue';
 
 const selectedNode = defineModel('selectedNode')
@@ -11,11 +11,12 @@ function toggleChildren() {
 function selectNode() {
   selectedNode.value = props.node
 }
+const isSelected = computed(() => selectedNode.value !== undefined && (selectedNode.value as TestNode).name == props.node.name)
 </script>
 
 <template>
   <button class="rounded px-2 bg-gray-200 mr-1" @click="toggleChildren" v-if="node.child"><span v-if="showChildren">-</span><span v-else>+</span></button>
-  <span class="cursor-pointer rounded p-px px-1" :class="{ 'bg-gray-300': selectedNode !== undefined && (selectedNode as TestNode).name == node.name }" @click="selectNode">{{ node.name }}</span>
+  <span class="cursor-pointer rounded p-px px-1" :class="{ 'bg-gray-200 font-bold': isSelected }" @click="selectNode">{{ node.name }}</span>
   <TestTree :roots="node.child" v-model:selectedNode="selectedNode" v-if="node.child" :class="{ hidden: !showChildren }"/>
 </template>
 
