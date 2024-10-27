781ecd2b85f4477d9f39783b6ce57f4698a62d1b
Marc Philipp <mail@marcphilipp.de>
2024-10-27 15:00:41 +0100

    Polishing

--- html-report/src/components/NodeDetails.vue
+++ html-report/src/components/NodeDetails.vue
@@ -10,7 +10,7 @@ const color = computed(() => props.node.result.status === 'SUCCESSFUL' ? 'green'
   <h2 class="text-xl mb-4">{{ node.name }}</h2>
   <div class="inline-flex mb-2 border-2 rounded-full px-2 py-1" :class="[`border-${color}-600`, `bg-${color}-500`]">
     <TestResultStatusIcon :status="node.result.status" class="text-white" />
-    <span class="ml-1 tracking-wide text-xs text-white">{{ node.result.status }}</span>
+    <span class="ml-1 tracking-wide text-xs text-white font-bold">{{ node.result.status }}</span>
   </div>
   <pre class="text-xs rounded-lg shadow-inner p-4 h-1/2 max-h-96 border w-full overflow-scroll">{{ node }}</pre>
 </template>
