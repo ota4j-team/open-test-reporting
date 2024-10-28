65ba1ac0de6930c4e05f47f6a01d9092cbdc7db8
Marc Philipp <mail@marcphilipp.de>
2024-10-28 16:07:50 +0100

    Format source information

---
+++ html-report/src/components/Section.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Block from './Block.vue'
+
+const { depth = 3 } = defineProps<{  section: Section, depth?: number }>()
+</script>
+
+<template>
+  <h3 v-if="depth === 3" class="text-lg font-bold mt-3 mb-1">{{ section.title }}</h3>
+  <h4 v-if="depth === 4" class="italic mt-2 mb-1">{{ section.title }}</h4>
+  <h5 v-if="depth >= 5">{{ section.title }}</h5>
+  <div v-for="block in section.blocks">
+    <Block :block="block" :depth="depth" />
+  </div>
+</template>
