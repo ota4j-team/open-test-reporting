d12ac3582f0853a800aca0c10bfa4e030c662c97
Marc Philipp <mail@marcphilipp.de>
2024-10-29 09:04:35 +0100

    Add duration

--- html-report/src/components/NodeDetails.vue
+++ html-report/src/components/NodeDetails.vue
@@ -2,6 +2,7 @@
 import { computed } from 'vue';
 import Section from './Section.vue';
 import TestResultStatusIcon from './TestResultStatusIcon.vue';
+import { Timer } from 'lucide-vue-next';
 
 const props = defineProps<{ node: TestNode }>()
 const color = computed(() => props.node.status === 'SUCCESSFUL' ? 'green' : 'red')
@@ -9,9 +10,13 @@ const color = computed(() => props.node.status === 'SUCCESSFUL' ? 'green' : 'red
 
 <template>
   <h2 class="text-xl mb-4">{{ node.name }}</h2>
-  <div class="inline-flex mb-2 border-2 rounded-full px-2 py-1" :class="[`border-${color}-600`, `bg-${color}-500`]">
+  <div class="inline-flex mb-2 border-2 rounded-full px-2 py-1 mr-2" :class="[`border-${color}-600`, `bg-${color}-500`]">
     <TestResultStatusIcon :status="node.status" class="text-white" />
     <span class="ml-1 tracking-wide text-xs text-white font-bold">{{ node.status }}</span>
+  </div>
+  <div class="inline-flex mb-2 border-2 rounded-full px-2 py-1 border-gray-400 bg-gray-300">
+    <Timer :size="16" :strokeWidth="3" class="self-center" />
+    <span class="ml-1 tracking-wide text-xs font-bold">{{ node.duration }}</span>
   </div>
   <div v-for="section in node.sections">
     <Section :section="section" />
