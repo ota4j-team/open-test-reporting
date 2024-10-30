2c0d47e1019bbdbcde60dfd17175d5932234dfdb
Marc Philipp <mail@marcphilipp.de>
2024-10-30 09:54:03 +0100

    Add breadcrumbs

--- html-report/src/TestNodeTree.ts
+++ html-report/src/TestNodeTree.ts
@@ -2,6 +2,7 @@ export default class TestNodeTree {
 
   private readonly rootIds: string[];
   private readonly childrenIds: Map<string, string[]>;
+  private readonly parentIds: Map<string, string>;
   private readonly testNodes: Map<string, TestNode>;
 
   constructor(
@@ -11,6 +12,10 @@ export default class TestNodeTree {
   ) {
     this.rootIds = rootIds
     this.childrenIds = childrenIds
+    this.parentIds = new Map<string, string>()
+    this.childrenIds.forEach((children, p) => {
+      children.forEach(c => this.parentIds.set(c, p))
+    });
     this.testNodes = new Map(testNodes.map(n => [n.id, n]))
   }
 
@@ -26,6 +31,15 @@ export default class TestNodeTree {
     if (this.childrenIds.has(node.id)) {
       return this.childrenIds.get(node.id)!!
         .map(id => this.testNodes.get(id)!!)
+    }
+    return []
+  }
+
+  parents(node: TestNode): TestNode[] {
+    if (this.parentIds.has(node.id)) {
+      const parentId = this.parentIds.get(node.id)!!
+      const parent = this.testNodes.get(parentId)!!
+      return [...this.parents(parent), parent]
     }
     return []
   }
--- html-report/src/components/NodeDetails.vue
+++ html-report/src/components/NodeDetails.vue
@@ -2,14 +2,25 @@
 import { computed } from 'vue';
 import Section from './Section.vue';
 import TestResultStatusIcon from './TestResultStatusIcon.vue';
-import { Timer } from 'lucide-vue-next';
+import { ChevronRight, Timer } from 'lucide-vue-next';
+import TestNodeTree from '../TestNodeTree';
 
-const props = defineProps<{ node: TestNode }>()
+const selectedNode = defineModel('selectedNode')
+const props = defineProps<{ node: TestNode, tree: TestNodeTree }>()
 const color = computed(() => props.node.status === 'SUCCESSFUL' ? 'green' : 'red')
+function selectNode(node: TestNode) {
+  selectedNode.value = node
+}
 </script>
 
 <template>
-  <h2 class="text-xl mb-4">{{ node.name }}</h2>
+  <ul class="text-sm mb-2">
+    <li v-for="parent in tree.parents(node)" class="inline-flex">
+      <span @click="selectNode(parent)" class="underline decoration-gray-300 decoration-2 cursor-pointer">{{ parent.name }}</span>
+      <ChevronRight :size="16" class="inline self-center mx-1 text-gray-600" />
+    </li>
+  </ul>
+  <h2 class="text-xl mb-4 font-bold">{{ node.name }}</h2>
   <div class="inline-flex mb-2 border-2 rounded-full px-2 py-1 mr-2" :class="[`border-${color}-600`, `bg-${color}-500`]">
     <TestResultStatusIcon :status="node.status" class="text-white" />
     <span class="ml-1 tracking-wide text-sm text-white font-bold">{{ node.status }}</span>
