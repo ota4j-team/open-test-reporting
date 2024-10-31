954b31983f7097950a3f01f7759ca16bcf7783e5
Marc Philipp <mail@marcphilipp.de>
2024-10-31 20:36:09 +0100

    Polishing

--- html-report/src/components/CodeBlock.vue
+++ html-report/src/components/CodeBlock.vue
@@ -9,8 +9,8 @@ const { copy, copied, isSupported } = useClipboard({ source })
 </script>
 
 <template>
-  <div class="flex flex-row rounded-lg bg-neutral-50 dark:bg-neutral-900 border-neutral-200 dark:border-neutral-700 border p-2">
-    <pre class="grow py-1 pl-1 text-sm h-1/2 max-h-96 w-full overflow-scroll"><code>{{ content }}</code></pre>
+  <div class="flex flex-row rounded-lg bg-neutral-50 dark:bg-neutral-900 border-neutral-200 dark:border-neutral-700 border p-1">
+    <pre class="grow py-2 pl-2 text-sm h-1/2 max-h-96 w-full overflow-scroll"><code>{{ content }}</code></pre>
     <div v-if="isSupported">
       <div class="cursor-pointer hover:bg-black/20 dark:hover:bg-white/20 rounded-lg p-1" :title="$t('clipboard.copy')" @click="copy(source)">
         <Copy v-if="!copied" :size="16" />
