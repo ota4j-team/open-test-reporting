28ec01e7a6c1759c314247c32957eb27b4118ce4
Marc Philipp <mail@marcphilipp.de>
2024-10-31 12:52:42 +0100

    Add tags

--- html-report/src/components/Block.vue
+++ html-report/src/components/Block.vue
@@ -5,16 +5,19 @@ defineProps<{ block: Block, depth: number }>()
 
 <template>
   <p v-if="block.type === 'p'">{{ block.content }}</p>
-  <table v-else-if="block.type === 'kvp'">
+  <table v-else-if="block.type === 'kvp'" class="w-full">
     <tbody>
       <tr v-for="(pair, index) in Object.entries(block.content)" :class="{'border-b': index < Object.entries(block.content).length - 1}" class="border-neutral-200 dark:border-neutral-700">
-        <td class="pr-4 text-sm">{{ pair[0] }}</td>
-        <td><code class="text-sm">{{ pair[1] }}</code></td>
+        <td class="pr-4 text-sm w-1/5">{{ pair[0] }}</td>
+        <td class="w-4/5"><code class="text-sm">{{ pair[1] }}</code></td>
       </tr>
     </tbody>
   </table>
   <div v-else-if="block.type === 'sub'" v-for="section in (block.content as Section[])">
     <Section :section="section" :depth="depth + 1" />
   </div>
+  <ul v-else-if="block.type == 'labels'">
+    <li v-for="label in (block.content as string[])" class="inline-block rounded bg-neutral-200 dark:bg-neutral-700 mr-2 px-2 py-0.5 text-sm">{{ label }}</li>
+  </ul>
   <pre v-else class="text-xs rounded-lg shadow-inner p-4 h-1/2 max-h-96 border w-full overflow-scroll">{{ block.content }}</pre>
 </template>
