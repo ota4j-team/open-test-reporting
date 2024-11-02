a3bcfdd65f16859d901f920672b2c97cea569518
Marc Philipp <mail@marcphilipp.de>
2024-11-02 12:48:14 +0100

    Make header and sidebar sticky

--- html-report/src/App.vue
+++ html-report/src/App.vue
@@ -12,12 +12,12 @@ const selectedNode = ref(props.tree.initialSelection());
 <template>
   <main class="flex flex-col h-screen bg-white dark:bg-black text-black dark:text-white">
     <StatusBar :tree="tree" />
-    <div class="flex grow">
-      <div class="bg-neutral-100 dark:bg-neutral-800 w-1/4 min-w-80 px-2 py-4 overflow-y-auto text-sm">
+    <div class="flex flex-1 overflow-hidden">
+      <div class="bg-neutral-100 dark:bg-neutral-800 w-1/4 min-w-80 px-2 py-4 overflow-scroll text-sm">
         <TestTree :tree="tree" :roots="tree.roots()" v-model:selectedNode="selectedNode" class="-ml-3"/>
       </div>
-      <div class="grow overflow-scroll">
-        <NodeDetails :tree="tree" :node="selectedNode" v-model:selectedNode="selectedNode" v-if="selectedNode"/>
+      <div class="flex-1 overflow-scroll">
+        <NodeDetails :tree="tree" :node="selectedNode" v-model:selectedNode="selectedNode" v-if="selectedNode" />
       </div>
     </div>
   </main>
