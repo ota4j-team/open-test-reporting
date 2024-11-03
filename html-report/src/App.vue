c8186d56a20138b197b784768b47179db91d8b0b
Marc Philipp <mail@marcphilipp.de>
2024-11-03 11:13:19 +0100

    Auto-hide scrollbars

--- html-report/src/App.vue
+++ html-report/src/App.vue
@@ -13,10 +13,10 @@ const selectedNode = ref(props.tree.initialSelection());
   <main class="flex flex-col h-screen bg-white dark:bg-black text-black dark:text-white">
     <StatusBar :tree="tree" />
     <div class="flex flex-1 overflow-hidden">
-      <div class="bg-neutral-100 dark:bg-neutral-800 w-1/4 min-w-80 px-2 py-4 overflow-scroll text-sm">
+      <div class="bg-neutral-100 dark:bg-neutral-800 w-1/4 min-w-80 px-2 py-4 overflow-auto text-sm">
         <TestTree :tree="tree" :roots="tree.roots()" v-model:selectedNode="selectedNode" class="-ml-3"/>
       </div>
-      <div class="flex-1 overflow-scroll">
+      <div class="flex-1 overflow-auto">
         <NodeDetails :tree="tree" :node="selectedNode" v-model:selectedNode="selectedNode" v-if="selectedNode" />
       </div>
     </div>
--- html-report/src/components/CodeBlock.vue
+++ html-report/src/components/CodeBlock.vue
@@ -10,7 +10,7 @@ const { copy, copied, isSupported } = useClipboard({ source })
 
 <template>
   <div class="flex flex-row rounded-lg bg-neutral-50 dark:bg-neutral-900 border-neutral-200 dark:border-neutral-700 border p-1">
-    <pre class="grow py-2 pl-2 text-sm h-1/2 max-h-96 w-full overflow-scroll" style="tab-size: 2"><code>{{ content }}</code></pre>
+    <pre class="grow py-2 pl-2 text-sm h-1/2 max-h-96 w-full overflow-auto" style="tab-size: 2"><code>{{ content }}</code></pre>
     <div v-if="isSupported">
       <div class="cursor-pointer hover:bg-black/20 dark:hover:bg-white/20 rounded-lg p-1" :title="$t('clipboard.copy')" @click="copy(source)">
         <Copy v-if="!copied" :size="16" />
