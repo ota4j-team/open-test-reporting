c06d43005ccb6bd16a9e904f992e2cffa30b3517
Marc Philipp <mail@marcphilipp.de>
2024-11-05 15:32:46 +0100

    Fix scrolling issue with code blocks

--- html-report/src/components/CodeBlock.vue
+++ html-report/src/components/CodeBlock.vue
@@ -13,10 +13,12 @@ const { isScrolling } = useScroll(el, { idle: 2000 })
 </script>
 
 <template>
-  <div class="relative rounded bg-neutral-50 dark:bg-neutral-900 border-neutral-200 dark:border-neutral-700 border">
+  <div class="z-0	relative rounded bg-neutral-50 dark:bg-neutral-900 border-neutral-200 dark:border-neutral-700 border">
     <pre class="resize-y p-3 text-sm w-full overflow-auto min-h-14 max-h-96" ref="el"><code>{{ content }}</code></pre>
-    <div class="absolute top-2 right-2" v-if="isSupported" :class="{'hidden': isScrolling}">
-      <div class="cursor-pointer bg-neutral-50 dark:bg-neutral-900 hover:bg-neutral-200 dark:hover:bg-neutral-700 border-neutral-200 dark:border-neutral-700 border rounded-lg p-1.5" :title="$t('clipboard.copy')" @click="copy(source)">
+    <div class="absolute top-2 right-2" v-if="isSupported" :class="{ 'hidden': isScrolling }">
+      <div
+        class="cursor-pointer bg-neutral-50 dark:bg-neutral-900 hover:bg-neutral-200 dark:hover:bg-neutral-700 border-neutral-200 dark:border-neutral-700 border rounded-lg p-1.5"
+        :title="$t('clipboard.copy')" @click="copy(source)">
         <Copy v-if="!copied" :size="16" />
         <Check v-else :size="16" />
       </div>
@@ -28,8 +30,9 @@ const { isScrolling } = useScroll(el, { idle: 2000 })
 pre {
   tab-size: 2;
 }
+
 /* Allow resizing beyond initial max height */
 pre[style*="height"] {
-  max-height: max-content; 
-} 
+  max-height: max-content;
+}
 </style>
