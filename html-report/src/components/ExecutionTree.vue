378c59f2d4ff317a623e7eeb999d5653f3cb5ff6
Marc Philipp <mail@marcphilipp.de>
2024-11-05 08:05:01 +0100

    Polishing

--- html-report/src/components/ExecutionTree.vue
+++ html-report/src/components/ExecutionTree.vue
@@ -13,7 +13,7 @@ defineProps<{ executions: TestExecution[] }>()
     <template #default="{ node }">
       <TreeNode :execution="(node as TestExecution)" :node="node" :children="(node as TestExecution).roots()" v-model:selectedNode="selectedNode">
         <template #icon="iconProps">
-          <SquareActivity v-bind="iconProps" class="text-neutral-600" />
+          <SquareActivity v-bind="iconProps" class="text-neutral-600 dark:text-neutral-300" />
         </template>
       </TreeNode>
     </template>
