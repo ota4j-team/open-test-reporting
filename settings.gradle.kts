c1d8c7632d2e9557dabbe11eae1dc04feb2eba78
Marc Philipp <mail@marcphilipp.de>
2024-11-06 10:08:28 +0100

    Set up Build Scan publication

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,33 @@
+plugins {
+    id("com.gradle.develocity") version "3.18.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.2"
+}
+
 rootProject.name = "open-test-reporting"
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
+    }
+}
+
+develocity {
+    buildScan {
+        val isCiServer = System.getenv("CI") != null
+
+        server = "https://ge.junit.org"
+        uploadInBackground = !isCiServer
+
+        obfuscation {
+            if (isCiServer) {
+                username { "github" }
+            } else {
+                hostname { null }
+                ipAddresses { emptyList() }
+            }
+        }
+
+        publishing.onlyIf { it.isAuthenticated }
     }
 }
 
