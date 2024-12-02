850ce8d14f6d341b6b11951fb749c7bd763a80e6
Marc Philipp <mail@marcphilipp.de>
2024-12-02 10:46:31 +0100

    Improve alignment

--- html-report/src/components/ToolBar.vue
+++ html-report/src/components/ToolBar.vue
@@ -11,7 +11,7 @@ const iconProps = {
 </script>
 
 <template>
-  <div class="sticky top-0 bg-neutral-100 dark:bg-neutral-800 text-neutral-600 dark:text-neutral-300 border-b border-neutral-200 dark:border-neutral-700 flex flex-row p-1.5">
+  <div class="sticky top-0 bg-neutral-100 dark:bg-neutral-800 text-neutral-600 dark:text-neutral-300 border-b border-neutral-200 dark:border-neutral-700 flex flex-row px-1.5 py-2">
     <div class="flex flex-row flex-1">
       <ToolBarIcon @click="rootStore.toggleShowSuccessful()" :selected="rootStore.showSuccessful" :title="$t('toolbar.showSuccessful')">
         <TestResultStatusIcon status="SUCCESSFUL" v-bind="iconProps" color="''"/>
