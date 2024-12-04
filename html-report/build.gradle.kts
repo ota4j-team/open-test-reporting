9e5d87495008370850bc07550216a4130f9a4e66
Marc Philipp <mail@marcphilipp.de>
2024-12-04 10:50:15 +0100

    Use Spotless to call Prettier to benefit from Gradle's input tracking

--- html-report/build.gradle.kts
+++ html-report/build.gradle.kts
@@ -1,5 +1,7 @@
+import com.diffplug.gradle.spotless.SpotlessApply
+import com.diffplug.gradle.spotless.SpotlessCheck
 import com.github.gradle.node.npm.task.NpmTask
-import com.github.gradle.node.npm.task.NpxTask
+import java.util.*
 
 plugins {
     base
@@ -15,34 +17,39 @@ node {
     npmInstallCommand = providers.environmentVariable("CI").map { "ci" }.orElse("install")
 }
 
-val prettierCheck by tasks.registering(NpxTask::class) {
-    dependsOn(tasks.npmInstall)
-    command = "prettier"
-    args.addAll(".", "--check")
-}
+val distIncludes = arrayOf("public/**", "src/**", "*.html", "*.js", "*.json", "*.ts")
 
-val prettierWrite by tasks.registering(NpxTask::class) {
-    dependsOn(tasks.npmInstall)
-    command = "prettier"
-    args.addAll(".", "--write")
-}
+spotless {
+    format("prettier") {
+        target(*distIncludes)
 
-tasks.spotlessCheck {
-    dependsOn(prettierCheck)
-}
+        val prettierVersion: String = resources.text.fromFile("package.json").asReader().useLines { lines ->
+            lines.first { it.contains("prettier") }
+        }.substringAfter("prettier\": \"").substringBefore("\"")
 
-tasks.spotlessApply {
-    dependsOn(prettierWrite)
+        prettier(prettierVersion).apply {
+            if (node.download.get()) {
+                val npmExec = if (System.getProperty("os.name").lowercase(Locale.ROOT).contains("windows")) "/npm.cmd" else "/bin/npm"
+                npmExecutable("${tasks.npmSetup.get().npmDir.get()}${npmExec}")
+            }
+        }
+    }
 }
 
 val buildVueDist by tasks.registering(NpmTask::class) {
     dependsOn(tasks.npmInstall)
+    shouldRunAfter("spotlessPrettierCheck")
     inputs.files(fileTree(node.nodeProjectDir) {
-        include("public/**", "src/**", "*.html", "*.js", "*.json", "*.ts")
+        include(*distIncludes)
+        exclude("public/init.js")
     })
     outputs.file(node.nodeProjectDir.file("dist/index.html"))
     outputs.cacheIf { true }
     npmCommand.addAll("run", "build")
+}
+
+tasks.named("spotlessPrettier") {
+    dependsOn(tasks.npmInstall)
 }
 
 configurations.consumable("htmlReportTemplate") {
