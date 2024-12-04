57bd6315dbf7fac4068f618797bba16a19307a94
Marc Philipp <mail@marcphilipp.de>
2024-12-04 09:35:54 +0100

    Rename RootUi to TreeState

--- html-report/src/components/sidebar/ToolBar.vue
+++ html-report/src/components/sidebar/ToolBar.vue
@@ -4,32 +4,32 @@ import ToolBarIcon from './ToolBarIcon.vue';
 import TestResultStatusIcon from "../common/TestResultStatusIcon.vue";
 import {defaultIconProps} from "../common/icon.ts";
 import {inject} from "vue";
-import {rootStoreKey} from "./keys.ts";
+import {treeStateKey} from "./TreeState.ts";
 
-const rootStore = inject(rootStoreKey)!!
+const treeState = inject(treeStateKey)!!
 </script>
 
 <template>
   <div class="sticky top-0 bg-neutral-100 dark:bg-neutral-800 text-neutral-600 dark:text-neutral-300 border-b border-neutral-200 dark:border-neutral-700 flex flex-row px-1.5 py-2">
     <div class="flex flex-row flex-1">
-      <ToolBarIcon @click="rootStore.toggleShowSuccessful()" :selected="rootStore.showSuccessful" :title="$t('toolbar.showSuccessful')">
+      <ToolBarIcon @click="treeState.toggleShowSuccessful()" :selected="treeState.showSuccessful" :title="$t('toolbar.showSuccessful')">
         <TestResultStatusIcon status="SUCCESSFUL" v-bind="defaultIconProps" color="''"/>
       </ToolBarIcon>
-      <ToolBarIcon @click="rootStore.toggleShowFailed()" :selected="rootStore.showFailed" :title="$t('toolbar.showFailed')">
+      <ToolBarIcon @click="treeState.toggleShowFailed()" :selected="treeState.showFailed" :title="$t('toolbar.showFailed')">
         <TestResultStatusIcon status="FAILED" v-bind="defaultIconProps" color="''"/>
       </ToolBarIcon>
-      <ToolBarIcon @click="rootStore.toggleShowSkipped()" :selected="rootStore.showSkipped" :title="$t('toolbar.showSkipped')">
+      <ToolBarIcon @click="treeState.toggleShowSkipped()" :selected="treeState.showSkipped" :title="$t('toolbar.showSkipped')">
         <TestResultStatusIcon status="SKIPPED" v-bind="defaultIconProps" color="''"/>
       </ToolBarIcon>
-      <ToolBarIcon @click="rootStore.toggleShowAborted()" :selected="rootStore.showAborted" :title="$t('toolbar.showAborted')">
+      <ToolBarIcon @click="treeState.toggleShowAborted()" :selected="treeState.showAborted" :title="$t('toolbar.showAborted')">
         <TestResultStatusIcon status="ABORTED" v-bind="defaultIconProps" color="''"/>
       </ToolBarIcon>
     </div>
     <div class="flex flex-row">
-      <ToolBarIcon @click="rootStore.expandAll()" :title="$t('toolbar.expandAll')">
+      <ToolBarIcon @click="treeState.expandAll()" :title="$t('toolbar.expandAll')">
         <ChevronsUpDown v-bind="defaultIconProps"/>
       </ToolBarIcon>
-      <ToolBarIcon @click="rootStore.collapseAll()" :title="$t('toolbar.collapseAll')">
+      <ToolBarIcon @click="treeState.collapseAll()" :title="$t('toolbar.collapseAll')">
         <ChevronsDownUp v-bind="defaultIconProps"/>
       </ToolBarIcon>
     </div>
--- html-report/src/components/sidebar/keys.ts
+++
@@ -1,4 +0,0 @@
-import {InjectionKey} from "vue";
-import RootUi from "./TreeState.ts";
-
-export const rootStoreKey = Symbol() as InjectionKey<RootUi>
