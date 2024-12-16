b0f0c8b48d25a6298326be6be66cb264e752a87e
Marc Philipp <mail@marcphilipp.de>
2024-12-16 09:58:26 +0100

    Increase header font size

--- html-report/src/components/details/DetailsHeader.vue
+++ html-report/src/components/details/DetailsHeader.vue
@@ -7,7 +7,7 @@ defineProps<{ title: string }>();
     class="z-10 sticky top-0 left-0 overflow-x-auto bg-neutral-100/50 dark:bg-neutral-800/50 backdrop-blur px-4 py-2.5"
   >
     <slot name="above" />
-    <h2 class="text-xl font-bold" role="heading" :aria-label="title">
+    <h2 class="text-2xl font-bold" role="heading" :aria-label="title">
       {{ title }}
     </h2>
     <slot name="below" />
