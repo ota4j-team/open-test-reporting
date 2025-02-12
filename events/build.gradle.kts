7aecc076661d6346d42701c952f01b90d46ac471
Marc Philipp <mail@marcphilipp.de>
2025-02-12 15:29:02 +0100

    Add Saxon test task

--- events/build.gradle.kts
+++ events/build.gradle.kts
@@ -8,6 +8,12 @@ val woodstoxRuntimeClasspath = configurations.resolvable("woodstoxRuntimeClasspa
     extendsFrom(woodstox.get())
 }
 
+val saxon = configurations.dependencyScope("saxon")
+val saxonRuntimeClasspath = configurations.resolvable("saxonRuntimeClasspath") {
+    extendsFrom(configurations.testRuntimeClasspath.get())
+    extendsFrom(saxon.get())
+}
+
 dependencies {
     api(projects.schema)
     compileOnlyApi(libs.apiguardian)
@@ -17,6 +23,7 @@ dependencies {
     testImplementation(libs.xmlunit.assertj)
     testCompileOnly(libs.jetbrains.annotations)
     woodstox(libs.woodstox.core)
+    saxon(libs.saxon)
 }
 
 tasks {
@@ -26,7 +33,13 @@ tasks {
         classpath = files(sourceSets.main.map { it.output }) + files(test.map { it.sources.output }) + woodstoxRuntimeClasspath.get()
         group = JavaBasePlugin.VERIFICATION_GROUP
     }
+    val testSaxon by registering(Test::class) {
+        val test by testing.suites.existing(JvmTestSuite::class)
+        testClassesDirs = files(test.map { it.sources.output.classesDirs })
+        classpath = files(sourceSets.main.map { it.output }) + files(test.map { it.sources.output }) + saxonRuntimeClasspath.get()
+        group = JavaBasePlugin.VERIFICATION_GROUP
+    }
     check {
-        dependsOn(testWoodstox)
+        dependsOn(testWoodstox, testSaxon)
     }
 }
