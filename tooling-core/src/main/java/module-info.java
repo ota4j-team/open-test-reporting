046bf03166e4aed3af848852980fd26f172e2943
Marc Philipp <mail@marcphilipp.de>
2025-02-12 17:41:40 +0100

    Polishing

--- tooling-core/src/main/java/module-info.java
+++ tooling-core/src/main/java/module-info.java
@@ -8,15 +8,15 @@
 module org.opentest4j.reporting.tooling.core {
 
     requires static org.apiguardian.api;
-    requires java.xml;
     requires com.google.gson;
     requires org.opentest4j.reporting.events;
-    requires org.opentest4j.reporting.schema;
     requires org.opentest4j.reporting.tooling.spi;
 
     exports org.opentest4j.reporting.tooling.core.converter;
     exports org.opentest4j.reporting.tooling.core.htmlreport;
     exports org.opentest4j.reporting.tooling.core.validator;
+
+    uses org.opentest4j.reporting.tooling.spi.htmlreport.Contributor;
 
     provides org.opentest4j.reporting.tooling.spi.htmlreport.Contributor with
             org.opentest4j.reporting.tooling.core.htmlreport.CoreContributor,
