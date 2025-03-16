6d4baf926151bd51fd9e3b890dbd12c62eb4aa62
Marc Philipp <mail@marcphilipp.de>
2025-03-16 12:17:19 +0100

    Move manifest config to shadowJar task
    The `jar --update --main-class` call updates both the module descriptor
    and the manifest.

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -12,27 +12,32 @@ dependencies {
     runtimeOnly(libs.log4j.slf4j2.impl)
 }
 
+val mainClass = "org.opentest4j.reporting.cli.ReportingCli"
+
 tasks {
     compileJava {
         options.release = 21
     }
     jar {
-        manifest {
-            attributes("Main-Class" to "org.opentest4j.reporting.cli.ReportingCli")
-        }
         doLast(objects.newInstance(UpdateJarAction::class).apply {
             javaLauncher = project.javaToolchains.launcherFor(java.toolchain)
             args.addAll(
                 "--file", archiveFile.get().asFile.absolutePath,
-                "--main-class", "org.opentest4j.reporting.cli.ReportingCli",
+                "--main-class", mainClass,
             )
         })
     }
     shadowJar {
+        manifest {
+            attributes("Main-Class" to mainClass)
+        }
         archiveClassifier = "standalone"
         exclude("META-INF/maven/**")
         exclude("META-INF/LICENSE*")
         exclude("META-INF/NOTICE*")
         exclude("**/module-info.class")
+    }
+    assemble {
+        dependsOn(shadowJar)
     }
 }
