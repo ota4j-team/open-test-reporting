d2baf4cff6ca518b88c1fa03e0fe14b1abc3e2b8
Marc Philipp <mail@marcphilipp.de>
2025-05-14 16:47:42 +0200

    Use nmcp plugin's task for publishing snapshots

--- buildSrc/src/main/kotlin/publishing-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/publishing-conventions.gradle.kts
@@ -50,14 +50,4 @@ publishing {
             }
         }
     }
-    repositories {
-        maven {
-            name = "mavenCentralSnapshots"
-            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
-            credentials {
-                username = providers.gradleProperty("mavenCentralUsername").orNull
-                password = providers.gradleProperty("mavenCentralPassword").orNull
-            }
-        }
-    }
 }
