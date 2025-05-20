334781f75f51342518e787a4fa1f4cb68729df7e
Marc Philipp <mail@marcphilipp.de>
2025-05-20 14:44:34 +0200

    Avoid Immutables annotation from leaking into JAR

--- tooling-spi/build.gradle.kts
+++ tooling-spi/build.gradle.kts
@@ -12,4 +12,8 @@ tasks {
     compileJava {
         options.compilerArgs.add("-Aimmutables.gradle.incremental")
     }
+    jar {
+        // Avoid Immutables annotation from leaking into JAR
+        exclude("org/opentest4j/reporting/tooling/spi/htmlreport/package-info.class")
+    }
 }
