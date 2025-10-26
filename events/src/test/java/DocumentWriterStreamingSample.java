c45eeef828199d4c84ae4b0bd472c2ba4fe4f335
Christoph Läubrich <mail@laeubi-soft.de>
2025-10-26 14:10:03 +0100

    Add support for sending events to a `Writer` directly (#728)
    Prior to this change, the `Event` class only accepted a file, but it 
    would be much more generic to use a writer and the documentation even 
    mention that use
    case.
    
    This now enhances the API with a new way to create a (event) writer that
    prints to the (Java) writer directly.
    
    ---------
    
    Co-authored-by: Marc Philipp <mail@marcphilipp.de>

---
+++ events/src/test/java/DocumentWriterStreamingSample.java
@@ -0,0 +1,53 @@
+
+/*
+ * Copyright 2021-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.opentest4j.reporting.events.api.DocumentWriter;
+import org.opentest4j.reporting.events.api.NamespaceRegistry;
+import org.opentest4j.reporting.events.core.CoreFactory;
+import org.opentest4j.reporting.events.root.Events;
+import org.opentest4j.reporting.schema.Namespace;
+
+import java.io.OutputStreamWriter;
+import java.time.Instant;
+
+import static org.opentest4j.reporting.events.core.CoreFactory.*;
+import static org.opentest4j.reporting.events.core.Result.Status.SUCCESSFUL;
+import static org.opentest4j.reporting.events.root.RootFactory.finished;
+import static org.opentest4j.reporting.events.root.RootFactory.started;
+
+public class DocumentWriterStreamingSample {
+
+	public static void main(String[] args) throws Exception {
+
+		NamespaceRegistry namespaceRegistry = NamespaceRegistry.builder(Namespace.REPORTING_CORE) // <1>
+				.add("e", Namespace.REPORTING_EVENTS) //
+				.add("git", Namespace.REPORTING_GIT) //
+				.add("java", Namespace.REPORTING_JAVA) //
+				.build();
+
+		// Example: streaming events to System.out (could be a socket, network stream, etc.)
+		try (DocumentWriter<Events> writer = Events.createDocumentWriter(namespaceRegistry,
+			new OutputStreamWriter(System.out))) {
+			writer.append(infrastructure(), infrastructure -> infrastructure // <2>
+					.append(hostName("wonderland")) //
+					.append(userName("alice")));
+			writer.append(started("1", Instant.now(), "container")); // <3>
+			writer.append(started("2", Instant.now(), "test"), started -> started.withParentId("1")); // <4>
+			writer.append(finished("2", Instant.now()), finished -> finished.append(CoreFactory.result(SUCCESSFUL))); // <5>
+			writer.append(finished("1", Instant.now()), finished -> finished.append(CoreFactory.result(SUCCESSFUL))); // <6>
+		}
+	}
+}
