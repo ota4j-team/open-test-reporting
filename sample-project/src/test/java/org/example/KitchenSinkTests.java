e95bb010833a05f460cf411c6607f21434bfd488
Marc Philipp <mail@marcphilipp.de>
2025-11-04 18:29:09 +0100

    Try to reproduce issue on Windows

--- sample-project/src/test/java/org/example/KitchenSinkTests.java
+++ sample-project/src/test/java/org/example/KitchenSinkTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.TestReporter;
 
@@ -58,10 +59,11 @@ public class KitchenSinkTests {
     @Test
     @Tag("failure")
     @Order(4)
-    void failed(TestReporter reporter) {
+    void failed(TestInfo testInfo, TestReporter reporter) {
         Map<String, String> map = new LinkedHashMap<>();
-        map.put("foo", "bar");
-        map.put("baz", "qux");
+        map.put("displayName", testInfo.getDisplayName());
+        map.put("testClass", testInfo.getTestClass().orElseThrow().getName());
+        map.put("testMethod", testInfo.getTestMethod().orElseThrow().toGenericString());
         reporter.publishEntry(map);
 
         MediaType mediaType = MediaType.create("text", "plain", ISO_8859_1);
