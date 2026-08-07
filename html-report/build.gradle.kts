8ea089c0fda4730e498658e0c8c0455715d5da31
Marc Philipp <mail@marcphilipp.de>
2026-08-07 08:34:23 +0200

    Use npm v12 to reduce supply chain risk (#1183)
    * Configure npm >=12 as mandatory engine
    * Use corepack to install npm
    * Use same versions for Gradle setup
    * Use `npx` to run `prettier`

--- html-report/build.gradle.kts
+++ html-report/build.gradle.kts
@@ -5,38 +5,36 @@ import java.util.*
 plugins {
     base
     alias(libs.plugins.node)
-    id("com.diffplug.spotless")
 }
 
 node {
     download = providers.gradleProperty("openTestReporting.nodeDownload").map { it.toBoolean() }.orElse(true)
-    version = providers.fileContents(layout.projectDirectory.file(".tool-versions")).asText.map {
-        it.substringAfter("nodejs").trim()
-    }
+    version = "24.19.0"
+    npmVersion = "12.0.2"
     npmInstallCommand = providers.environmentVariable("CI").map { "ci" }.orElse("install")
 }
 
 val distIncludes = arrayOf("public/**", "src/**", "*.html", "*.js", "*.json", "*.ts")
-
-spotless {
-    format("prettier") {
-        target(*distIncludes)
-        prettier(readVersionFromPackageJson("prettier")).apply {
-            if (node.download.get()) {
-                val npmExec = if (System.getProperty("os.name").lowercase(Locale.ROOT).contains("windows")) "/npm.cmd" else "/bin/npm"
-                npmExecutable("${tasks.npmSetup.get().npmDir.get()}${npmExec}")
-            }
-        }
-    }
-}
 
 val eslintCheck by tasks.registering(NpxTask::class) {
     dependsOn(tasks.npmInstall)
     command = "eslint"
 }
 
+val prettierCheck by tasks.registering(NpxTask::class) {
+    dependsOn(tasks.npmInstall)
+    command = "prettier"
+    args.addAll(".", "--check")
+}
+
+val prettierWrite by tasks.registering(NpxTask::class) {
+    dependsOn(tasks.npmInstall)
+    command = "prettier"
+    args.addAll(".", "--check")
+}
+
 tasks.check {
-    dependsOn(eslintCheck)
+    dependsOn(eslintCheck, prettierCheck)
 }
 
 val eslintFix by tasks.registering(NpxTask::class) {
@@ -47,7 +45,7 @@ val eslintFix by tasks.registering(NpxTask::class) {
 
 val buildVueDist by tasks.registering(NpmTask::class) {
     dependsOn(tasks.npmInstall)
-    shouldRunAfter("spotlessPrettierCheck")
+    shouldRunAfter(prettierCheck)
     inputs.files(fileTree(node.nodeProjectDir) {
         include(*distIncludes)
         exclude("public/init.js")
@@ -55,10 +53,6 @@ val buildVueDist by tasks.registering(NpmTask::class) {
     outputs.file(node.nodeProjectDir.file("dist/index.html"))
     outputs.cacheIf { true }
     npmCommand.addAll("run", "build")
-}
-
-tasks.named("spotlessPrettier") {
-    dependsOn(tasks.npmInstall)
 }
 
 configurations.consumable("htmlReportTemplate") {
@@ -69,7 +63,3 @@ configurations.consumable("htmlReportTemplate") {
         attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named(LibraryElements.RESOURCES))
     }
 }
-
-fun readVersionFromPackageJson(packageName: String) = resources.text.fromFile("package.json").asReader().useLines { lines ->
-        lines.first { it.contains("\"${packageName}\":") }
-    }.substringAfter("\"${packageName}\": \"").substringBefore("\"")
